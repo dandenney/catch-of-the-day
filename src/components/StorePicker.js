f1e6e0f26b9c664301bb076530d6d5639d7794cf
Dan Denney <dd.subscriptions@gmail.com>
2017-02-13 14:11:55 -0500

    feat: Add the first component
    Learned the basic structure to put Hello on a page

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+
+  // Every component needs a render method
+  render() {
+    return <p>Hello</p>
+  }
+
+}
+
+export default StorePicker;

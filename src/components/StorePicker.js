d052140935cd93ebb24995fcf0335bd8246e7b4d
Dan Denney <dd.subscriptions@gmail.com>
2017-02-15 13:13:48 -0500

    feat: Use a helper for store names
    Learned about the named import/exports

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component {
 
   // Every component needs a render method
   render() {
     return (
-      <form className="">
+      <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input placeholder="Store Name" required type="text" />
+        <input defaultValue={ getFunName() } placeholder="Store Name" required type="text" />
         <button type="submit">Visit Store →</button>
       </form>
     )

86e1b8a5ad1b1bf8cf3e60e5394db89cad3c5380
Dan Denney <dd.subscriptions@gmail.com>
2017-02-15 13:07:48 -0500

    feat: Add routing via React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-import StorePicker from './components/StorePicker';
 import App from './components/App';
+import NotFound from './components/NotFound';
+import StorePicker from './components/StorePicker';
+
 import './css/style.css';
 
-render(<App/>, document.querySelector('#main'));
+// Routing, using react-router
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <routes>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </routes>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));

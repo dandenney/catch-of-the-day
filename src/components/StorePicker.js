27b2b298ef57b1eefada9b0cefb5eaceddaeec55
Dan Denney <dd.subscriptions@gmail.com>
2017-02-16 10:36:49 -0500

    feat: Get the value of the input without touching the DOM

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,15 +6,16 @@ class StorePicker extends React.Component {
 
   goToStore(e) {
     e.preventDefault();
+    console.log(this.storeInput.value);
     console.log('URL changed');
   }
 
   // Every component needs a render method
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input defaultValue={ getFunName() } placeholder="Store Name" required type="text" />
+        <input defaultValue={ getFunName() } placeholder="Store Name" ref={(input) => { this.storeInput = input}} required type="text" />
         <button type="submit">Visit Store →</button>
       </form>
     )

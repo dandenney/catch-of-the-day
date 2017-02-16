e351591aa6e18d9838fa868149d3ac1a0bd5380d
Dan Denney <dd.subscriptions@gmail.com>
2017-02-16 09:31:33 -0500

    feat: Prevent default form submission

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,10 +4,15 @@ import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component {
 
+  goToStore(e) {
+    e.preventDefault();
+    console.log('URL changed');
+  }
+
   // Every component needs a render method
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input defaultValue={ getFunName() } placeholder="Store Name" required type="text" />
         <button type="submit">Visit Store →</button>

c19bbfde4cdcc87e4e6a2e76342711f4f4b9089a
Dan Denney <dd.subscriptions@gmail.com>
2017-02-21 14:18:35 -0500

    feat: Sync state with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCyeYwzqB-fst6M21ZdmvzdevFnB7IAlHo",
+  authDomain: "catch-of-the-day-dan-denney.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-dan-denney.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Inventory from './Inventory';
 import Order from './Order';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 
 class App extends React.Component {
@@ -20,6 +21,21 @@ class App extends React.Component {
       fishes: {},
       order: {}
     };
+  }
+
+  componentWillMount() {
+
+    this.ref = base.syncState( `${ this.props.params.storeId }/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+
+  }
+
+  componentWillUnmount() {
+
+    base.removeBinding( this.ref );
+
   }
 
   addFish(fish) {

ef5e41c897756ed24c3575493236ed356ca6980e
Dan Denney <dd.subscriptions@gmail.com>
2017-03-21 20:00:58 -0400

    feat: Add auth and restrict edits to store owners
    We're done! This was so awesome.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
@@ -6,9 +6,7 @@ import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
 import base from '../base';
 
-
 class App extends React.Component {
-
   constructor() {
     super();
 
@@ -22,126 +20,121 @@ class App extends React.Component {
     // Initial state
     this.state = {
       fishes: {},
-      order: {}
+      order: {},
     };
   }
 
   componentWillMount() {
-
     // this runs right before the <App> is rendered
-    this.ref = base.syncState( `${ this.props.params.storeId }/fishes`, {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
       context: this,
-      state: 'fishes'
+      state: 'fishes',
     });
 
     // check if there is any order in localStorage
-    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    const localStorageRef = localStorage.getItem(
+      `order-${this.props.params.storeId}`,
+    );
 
     if (localStorageRef) {
-
       // update our App component's order state
       this.setState({
-        order: JSON.parse(localStorageRef)
+        order: JSON.parse(localStorageRef),
       });
-
     }
-
   }
 
   componentWillUnmount() {
-
-    base.removeBinding( this.ref );
-
+    base.removeBinding(this.ref);
   }
 
-  componentWillUpdate( nextProps, nextState ) {
-
-    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
-
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(
+      `order-${this.props.params.storeId}`,
+      JSON.stringify(nextState.order),
+    );
   }
 
   addFish(fish) {
     // update our state
-    const fishes = { ...this.state.fishes };
+    const fishes = {...this.state.fishes};
     // add in our new fish
     const timestamp = Date.now();
     fishes[`fish-${timestamp}`] = fish;
     // set state
-    this.setState({ fishes });
+    this.setState({fishes});
   }
 
   updateFish(key, updatedFish) {
-
-    const fishes = { ...this.state.fishes };
+    const fishes = {...this.state.fishes};
     fishes[key] = updatedFish;
-    this.setState({ fishes });
-
+    this.setState({fishes});
   }
 
   removeFish(key) {
-
-    const fishes = { ...this.state.fishes };
+    const fishes = {...this.state.fishes};
     fishes[key] = null;
-    this.setState({ fishes });
-
+    this.setState({fishes});
   }
 
   loadSamples() {
     this.setState({
-      fishes: sampleFishes
-    })
+      fishes: sampleFishes,
+    });
   }
 
   addToOrder(key) {
-
     // take a copy of our state
-    const order = { ...this.state.order };
+    const order = {...this.state.order};
     // update or add the new number of fish ordered
     order[key] = order[key] + 1 || 1;
     // update our state
-    this.setState({ order });
-
+    this.setState({order});
   }
 
   removeFromOrder(key) {
-
-    const order = { ...this.state.order };
+    const order = {...this.state.order};
     delete order[key];
-    this.setState({ order });
-
+    this.setState({order});
   }
 
   render() {
-    return(
-      <div className='catch-of-the-day'>
-        <div className='menu'>
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
           <Header tagline="Catch of the Day" />
           <ul className="list-of-fishes">
-            {
-              Object
-                .keys( this.state.fishes )
-                .map( key => <Fish addToOrder={ this.addToOrder } index={ key } key={ key } details={ this.state.fishes[ key ] } /> )
-            }
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                addToOrder={this.addToOrder}
+                index={key}
+                key={key}
+                details={this.state.fishes[key]}
+              />
+            ))}
           </ul>
         </div>
         <Order
-          fishes={ this.state.fishes }
-          order={ this.state.order }
-          removeFromOrder={ this.removeFromOrder }
-          params={ this.props.params }/>
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+          params={this.props.params}
+        />
         <Inventory
-          addFish={ this.addFish }
-          fishes={ this.state.fishes }
-          loadSamples={ this.loadSamples }
-          removeFish={ this.removeFish }
-          updateFish={ this.updateFish } />
+          addFish={this.addFish}
+          fishes={this.state.fishes}
+          loadSamples={this.loadSamples}
+          removeFish={this.removeFish}
+          updateFish={this.updateFish}
+          storeId={this.props.params.storeId}
+        />
       </div>
-    )
+    );
   }
 }
 
 App.propTypes = {
-  params: React.PropTypes.object.isRequired
-}
+  params: React.PropTypes.object.isRequired,
+};
 
 export default App;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,69 +1,179 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
+import base from '../base';
 
 class Inventory extends React.Component {
-
   constructor() {
-
     super();
     this.renderInventory = this.renderInventory.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+    this.authenticate = this.authenticate.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+    this.logout = this.logout.bind(this);
 
+    this.state = {
+      uid: null,
+      owner: null,
+    };
+  }
+
+  componentDidMount() {
+    base.onAuth(user => {
+      if (user) {
+        this.authHandler(null, {user});
+      }
+    });
   }
 
   handleChange(e, key) {
-
     const fish = this.props.fishes[key];
 
     // take a copy of the fish and update it with the new data
 
-    const updatedFish = { ...fish, [ e.target.name ]: e.target.value }
+    const updatedFish = {...fish, [e.target.name]: e.target.value};
     this.props.updateFish(key, updatedFish);
+  }
 
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>
+          Log in with Github
+        </button>
+      </nav>
+    );
+  }
+
+  authenticate(provider) {
+    console.log(`Trying to log in with ${provider}`);
+    base.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  logout() {
+    base.unauth();
+    this.setState({uid: null});
+  }
+
+  authHandler(err, authData) {
+    console.log(authData);
+
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Grab the store info
+    const storeRef = base.database().ref(this.props.storeId);
+
+    // Query firebase once
+    storeRef.once('value', snapshot => {
+      const data = snapshot.val() || {};
+
+      // Claim it
+      if (!data.owner) {
+        storeRef.set({
+          owner: authData.user.uid,
+        });
+      }
+
+      this.setState({
+        uid: authData.user.uid,
+        owner: data.owner || authData.user.uid,
+      });
+    });
   }
 
   renderInventory(key) {
-
     const fish = this.props.fishes[key];
     return (
-      <div className='fish-edit' key={key}>
+      <div className="fish-edit" key={key}>
 
-        <input type='text' name='name' onChange={ (e) => this.handleChange( e, key )} placeholder='Fish Name' value={ fish.name } />
-        <input type='text' name='price' onChange={ (e) => this.handleChange( e, key )} placeholder='Fish Price' value={ fish.price } />
+        <input
+          type="text"
+          name="name"
+          onChange={e => this.handleChange(e, key)}
+          placeholder="Fish Name"
+          value={fish.name}
+        />
+        <input
+          type="text"
+          name="price"
+          onChange={e => this.handleChange(e, key)}
+          placeholder="Fish Price"
+          value={fish.price}
+        />
 
-        <select type='text' name='status' onChange={ (e) => this.handleChange( e, key )} placeholder='Fish Status' value={ fish.status }>
-          <option value='available'>Fresh!</option>
-          <option value='unavailable'>Sold Out!</option>
+        <select
+          type="text"
+          name="status"
+          onChange={e => this.handleChange(e, key)}
+          placeholder="Fish Status"
+          value={fish.status}
+        >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
         </select>
 
-        <textarea type='text' name='description' onChange={ (e) => this.handleChange( e, key )} placeholder='Fish Description' value={ fish.description }></textarea>
-        <input type='text' name='image' onChange={ (e) => this.handleChange( e, key )} placeholder='Fish Image' value={ fish.image } />
+        <textarea
+          type="text"
+          name="description"
+          onChange={e => this.handleChange(e, key)}
+          placeholder="Fish Description"
+          value={fish.description}
+        />
+        <input
+          type="text"
+          name="image"
+          onChange={e => this.handleChange(e, key)}
+          placeholder="Fish Image"
+          value={fish.image}
+        />
 
         <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
 
       </div>
-    )
-
+    );
   }
 
   render() {
+    const logout = <button onClick={this.logout}>Log Out!</button>;
+
+    // Check for a logged in person
+    if (!this.state.uid) {
+      return <div>{this.renderLogin()}</div>;
+    }
+
+    // Check if logges in person is store owner
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>No and then</p>
+          {logout}
+        </div>
+      );
+    }
+
     return (
       <section>
         <h2>Inventory</h2>
+        {logout}
         {Object.keys(this.props.fishes).map(this.renderInventory)}
-        <AddFishForm addFish={ this.props.addFish } />
+        <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </section>
-
-    )
+    );
   }
 }
 
-Inventory.propTypes= {
+Inventory.propTypes = {
   fishes: React.PropTypes.object.isRequired,
   updateFish: React.PropTypes.func.isRequired,
   removeFish: React.PropTypes.func.isRequired,
   addFish: React.PropTypes.func.isRequired,
-  loadSamples: React.PropTypes.func.isRequired
-}
+  loadSamples: React.PropTypes.func.isRequired,
+  storeId: React.PropTypes.string.isRequired,
+};
 
 export default Inventory;
